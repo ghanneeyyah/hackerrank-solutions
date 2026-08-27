4641241f0f3f05049714b04e4708ad0deb1114cb
Ganiyat Olaiwon <olaiwonganiyat18@gmail.com>
2026-08-27 22:48:01 +0100

    ✅ Java Regex 2 - Duplicate Words [java8] — approach1

---
+++ solutions/java8/medium/duplicate-word/solution-approach1.java
@@ -0,0 +1,42 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/duplicate-word/problem?isFullScreen=true
+// Problem     Java Regex 2 - Duplicate Words
+// Difficulty  Medium
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-27, 10:47 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicateWords {
+
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll("(?i)"+ m.group(), m.group(1));
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
+    }
+}
