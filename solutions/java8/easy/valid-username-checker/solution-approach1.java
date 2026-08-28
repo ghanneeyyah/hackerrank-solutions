6389a3748967e7d85e36433ae3d56f00b5274a71
Ganiyat Olaiwon <olaiwonganiyat18@gmail.com>
2026-08-28 22:35:58 +0100

    ✅ Valid Username Regular Expression [java8] — approach1

---
+++ solutions/java8/easy/valid-username-checker/solution-approach1.java
@@ -0,0 +1,20 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/valid-username-checker/problem?isFullScreen=true
+// Problem     Valid Username Regular Expression
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-28, 10:35 p.m.
+// ──────────────────────────────────────────────────
+
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression ="\\A[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+    //public static final String regularExpression = null;
+}
+
