9ab1e06d2f7083cce262ef5cc7a3569d20fd4957
Ganiyat Olaiwon <olaiwonganiyat18@gmail.com>
2026-08-30 11:16:30 +0100

    ✅ Solve Me First [java15] — approach1

---
+++ solutions/java15/easy/solve-me-first/solution-approach1.java
@@ -0,0 +1,33 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+// Problem     Solve Me First
+// Difficulty  Easy
+// Subdomain   Warmup
+// Platform    HackerRank
+// Language    java15
+// Status      Accepted
+// Submitted   2026-08-30, 11:16 a.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+      // Hint: Type return a+b; below 
+      return a+b;
+	}
+
+  
+   public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        in.close();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+	}
+}
